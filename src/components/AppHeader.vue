7946eb107c8cee3c708ad45edfbd9fff5b7bc148
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-12-21 14:48:40 +0100

    AppHeader layout setup

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <nav>
+            <img src="../assets/dc-logo.png" alt="">
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+</style>
+
