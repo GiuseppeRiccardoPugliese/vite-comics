a5f2e11b381b6eb98e05beaa064afae0c02c1c92
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-12-22 16:28:01 +0100

    Creazione componente per le card dei fumetti, con i details presi dinamicamente & layout per il contenuto

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,13 +1,107 @@
 <script>
+import AppThumbs from './AppThumbs.vue'
+
 export default {
     name: 'AppContent',
+    components: {
+        AppThumbs,
+    },
+    data() {
+        return {
+            thumbs: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel",
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book",
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel",
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <main>
+        <div>
+            <img class="fill" src="/public/jumbotron.jpg" alt="">
+        </div>
         <div class="container">
-            <h2> ... Content goes here ...</h2>
+            <div class="btn">
+                <h3>CURRENT SERIES</h3>
+            </div>
+            <div class="cards">
+                <AppThumbs v-for="(thumb, i) in thumbs" :key="i" :details="thumb" />
+            </div>
+            <div class="btn load_more">
+                <a href="#">LOAD MORE</a>
+            </div>
         </div>
     </main>
 </template>
@@ -18,18 +112,56 @@ export default {
 
 main {
     background-color: $bg-black;
-    height: 130px;
+    padding: 10px;
 
-    .container {
-        width: 100%;
+    div {
         height: 100%;
-        display: flex;
-        align-items: center;
+        width: 100%;
+
+        img {
+            width: 100%;
+            height: 400px;
+
+            &.fill {
+                object-fit: cover;
+                object-position: top;
+            }
+        }
     }
 
+    .container {
+        .btn {
+            padding: 10px 20px;
+            background-color: $blue;
+            color: $primary;
+            width: fit-content;
+            position: relative;
+            bottom: 25px;
 
-    h2 {
-        color: $primary;
+            &.load_more {
+                margin: 0 auto;
+                bottom: 0;
+
+                & a {
+                    text-decoration: none;
+                    color: $primary;
+                    font-weight: 600;
+                }
+            }
+        }
+
+        .cards {
+            width: 100%;
+            padding: 20px 0;
+            display: flex;
+            flex-wrap: wrap;
+
+            h2 {
+                color: $primary;
+            }
+        }
+
+
     }
 }
 </style>
---
+++ src/components/AppThumbs.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'AppThumbs',
+    props: {
+        details: Object
+    }
+}
+</script>
+
+<template>
+    <div class="thumb-card">
+        <a href="#">
+            <img :src="details.thumb" :alt="details.type">
+            <h3>{{ details.series }}</h3>
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.thumb-card {
+    width: calc((100% / 6) - 1rem);
+    margin: 0.5rem;
+
+    // flex-basis: 15%;
+    a {
+        text-decoration: none;
+
+        h3 {
+            color: $primary;
+            // padding-bottom: 10px;
+            margin: 1rem 0;
+        }
+
+        img {
+            width: 200px;
+            height: 250px;
+        }
+    }
+}
+</style>
+
